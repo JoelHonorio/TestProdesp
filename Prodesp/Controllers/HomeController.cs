7a0e1d63003e0d8b37240bbdaeb495ddeff25ebe
joel <joel@seeger.com.br>
2022-07-29 00:44:54 -0300

    Ajustes de organizações

--- Prodesp/Controllers/HomeController.cs
+++ Prodesp/Controllers/HomeController.cs
@@ -1,13 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿#region Using
+
+using Microsoft.AspNetCore.Mvc;
+
+#endregion
 
 namespace Prodesp.Controllers
 {
     public class HomeController : Controller
     {
+        #region Injeção de dependências
+
         public HomeController()
         {
 
         }
+
+        #endregion
 
         public IActionResult Index()
         {
