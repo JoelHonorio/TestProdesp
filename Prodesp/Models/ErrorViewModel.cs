3e1664737bb5213dfb512a8997902c932225e088
joel <joel@seeger.com.br>
2022-07-29 00:30:36 -0300

    Criação da base do projeto

---
+++ Prodesp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Prodesp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
