429e513c1f19be73189b6d14dff2d3adfb244802
joel <joel@seeger.com.br>
2022-07-29 00:41:33 -0300

    Remoção de códigos que não serão utilizados

--- Prodesp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Prodesp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
