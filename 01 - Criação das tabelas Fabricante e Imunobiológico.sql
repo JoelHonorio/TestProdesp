8d7b237fc9ebd5217baf699f51c8c372c2c73830
joel <joel@seeger.com.br>
2022-07-29 18:37:51 -0300

    Gerando Migração, e testando no banco de dados

---
+++ 01 - Criação das tabelas Fabricante e Imunobiológico.sql
@@ -0,0 +1,43 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Fabricante] (
+    [Id] uniqueidentifier NOT NULL,
+    [Marca] nvarchar(50) NOT NULL,
+    [DataCriacao] datetime NOT NULL,
+    [DataModificacao] datetime NOT NULL,
+    CONSTRAINT [PK_Fabricante] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [Imunobiologico] (
+    [Id] uniqueidentifier NOT NULL,
+    [FabricanteId] uniqueidentifier NOT NULL,
+    [Descricao] nvarchar(400) NOT NULL,
+    [AnoLote] int NOT NULL,
+    [DataCriacao] datetime NOT NULL,
+    [DataModificacao] datetime NOT NULL,
+    CONSTRAINT [PK_Imunobiologico] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Imunobiologico_Fabricante_FabricanteId] FOREIGN KEY ([FabricanteId]) REFERENCES [Fabricante] ([Id]) ON DELETE NO ACTION
+);
+GO
+
+CREATE INDEX [IX_Imunobiologico_FabricanteId] ON [Imunobiologico] ([FabricanteId]);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20220729210158_Initial', N'6.0.7');
+GO
+
+COMMIT;
+GO
---
+++ Prodesp.Dados/Migrations/20220729210158_Initial.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Prodesp.Dados.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Fabricante",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Marca = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    DataCriacao = table.Column<DateTime>(type: "datetime", nullable: false),
+                    DataModificacao = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fabricante", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Imunobiologico",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FabricanteId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(400)", nullable: false),
+                    AnoLote = table.Column<int>(type: "int", nullable: false),
+                    DataCriacao = table.Column<DateTime>(type: "datetime", nullable: false),
+                    DataModificacao = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Imunobiologico", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Imunobiologico_Fabricante_FabricanteId",
+                        column: x => x.FabricanteId,
+                        principalTable: "Fabricante",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Imunobiologico_FabricanteId",
+                table: "Imunobiologico",
+                column: "FabricanteId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Imunobiologico");
+
+            migrationBuilder.DropTable(
+                name: "Fabricante");
+        }
+    }
+}
