e6f8e87e4a73c64777d395a5808bab377d861d69
joel <joel@seeger.com.br>
2022-07-31 12:05:19 -0300

    Script de inserção de dados

---
+++ 02 - Inserção para a tabela Fabricante.sql
@@ -0,0 +1,33 @@
+﻿INSERT INTO 
+	[dbo].[Fabricante]
+	(
+		[Id]
+		,[Marca]
+		,[DataCriacao]
+		,[DataModificacao]
+	)
+VALUES
+	(
+		'7ac53a99-07b2-481f-a30c-7017b891e286'
+		,'Pfizer'
+		,GETDATE()
+		,GETDATE()
+	)
+GO
+
+INSERT INTO 
+	[dbo].[Fabricante]
+    (
+		[Id]
+        ,[Marca]
+        ,[DataCriacao]
+        ,[DataModificacao]
+	)
+VALUES
+    (
+		'f064b162-7e84-446a-9e74-fc41620cd8bb'
+        ,'Sinovac'
+        ,GETDATE()
+        ,GETDATE()
+	)
+GO
